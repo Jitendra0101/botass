80329f3fa445dbf5919e53f1d42aeba8691389a3
diabolical-shit <73271099+diabolical-shit@users.noreply.github.com>
2020-10-22 10:56:47 +0530

    mybot

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="")
+
+@client.event
+async def on_ready():
+    print("bot is up for shit")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("hey there punk")
+
+@client.command()
+async def stfu(ctx):
+    await ctx.send("no you shut the fuck up whore")
+
+@client.command()
+async def f(ctx):
+    await ctx.send("pay respect")
+
+@client.command()
+async def bruh(ctx):
+    await ctx.send("bruh moment certified")
+    
+client.run("NzY4NDkzOTgyMDYwNjQyMzA0.X5BRyw.EkfBk2httVV0Kbfyn0oxUKZc5g4")
